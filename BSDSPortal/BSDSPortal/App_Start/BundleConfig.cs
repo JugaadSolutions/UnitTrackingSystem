ec74b9373b63d614a841e872964f2ce515128044
Avinash52 <avinashj.dream@gmail.com>
2015-07-03 13:32:01 +0530

    Jquery ui tab fix

--- BSDSPortal/BSDSPortal/App_Start/BundleConfig.cs
+++ BSDSPortal/BSDSPortal/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace BSDSPortal
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
